8996acd09ab0d129830fdc4894f369d1186a9c81
Jakaria Amin <jakariaamin@gmail.com>
2023-04-29 21:50:01 +0800

    Update CardNfcUtils.java
    Fix android build target 31 issue in pending intent.

--- creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
+++ creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
@@ -8,6 +8,7 @@ import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
 import android.nfc.tech.NfcA;
+import android.os.Build;
 
 /**
  * Created by pro100svitlo on 31.03.16.
@@ -26,8 +27,13 @@ public class CardNfcUtils {
     public CardNfcUtils(final Activity pActivity) {
         mActivity = pActivity;
         mNfcAdapter = NfcAdapter.getDefaultAdapter(mActivity);
-        mPendingIntent = PendingIntent.getActivity(mActivity, 0,
-                new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            mPendingIntent = PendingIntent.getActivity(mActivity, 0,
+                    new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        } else {
+            mPendingIntent = PendingIntent.getActivity(mActivity, 0,
+                    new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        }
     }
 
     public void disableDispatch() {
