cd49c28c803e41d94d96a182caf13c932b72c698
Jakaria Amin <jakariaamin@gmail.com>
2023-04-30 09:13:25 +0800

    Update CardNfcUtils.java

--- creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
+++ creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
@@ -27,13 +27,12 @@ public class CardNfcUtils {
     public CardNfcUtils(final Activity pActivity) {
         mActivity = pActivity;
         mNfcAdapter = NfcAdapter.getDefaultAdapter(mActivity);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            mPendingIntent = PendingIntent.getActivity(mActivity, 0,
-                    new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        } else {
-            mPendingIntent = PendingIntent.getActivity(mActivity, 0,
-                    new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        int flags = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            flags |= PendingIntent.FLAG_MUTABLE;
         }
+        mPendingIntent = PendingIntent.getActivity(mActivity, 0,
+                new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), flags);
     }
 
     public void disableDispatch() {
